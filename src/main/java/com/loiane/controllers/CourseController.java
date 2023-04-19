3d173b1ba71ea26d88e10f455f3ceddb4b367219
diegobizerra <diegobizerra@tjrj.jus.br>
2023-04-19 10:06:53 -0300

    primeiro comite do projeto

---
+++ src/main/java/com/loiane/controllers/CourseController.java
@@ -0,0 +1,68 @@
+package com.loiane.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.loiane.model.Course;
+import com.loiane.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/courses")
+public class CourseController {
+
+	private final CourseRepository courseRepository;
+
+	@GetMapping
+	public List<Course> list() {
+		return courseRepository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Course> findById(@PathVariable Long id) {
+		return courseRepository.findById(id).map(recordFound -> ResponseEntity.ok().body(recordFound))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@ResponseStatus(code = HttpStatus.CREATED)
+	@PostMapping()
+	public Course create(@RequestBody Course course) {
+		return courseRepository.save(course);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+		return courseRepository.findById(id).map(recordFound -> {
+			recordFound.setName(course.getName());
+			recordFound.setCategory(course.getCategory());
+			Course update = courseRepository.save(recordFound);
+			return ResponseEntity.ok().body(update);
+		}).orElse(ResponseEntity.notFound().build());
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		return courseRepository.findById(id)
+				.map(recordFound -> {
+			courseRepository.deleteById(id);
+			return ResponseEntity.noContent().<Void>build();
+		})
+		.orElse(ResponseEntity.notFound().build());
+	}
+
+}
---
+++ src/main/java/com/loiane/model/Course.java
@@ -0,0 +1,29 @@
+package com.loiane.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Course {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@JsonProperty("_id")
+	private Long id;
+	
+	@Column(length = 200, nullable = false)
+	private String name;
+	
+	@Column(length = 50, nullable = false)
+	private String category;
+	
+}
