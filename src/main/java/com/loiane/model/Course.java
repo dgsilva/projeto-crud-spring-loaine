ef6bc2e188de7884505b9e5c38501088cbcfb984
diegobizerra <diegobizerra@tjrj.jus.br>
2023-05-14 10:13:41 -0300

    Implementando Validation nas classe Course e CourseController.

--- src/main/java/com/loiane/model/Course.java
+++ src/main/java/com/loiane/model/Course.java
@@ -5,7 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -13,6 +19,8 @@ import lombok.Data;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' where id= ? ")
+@Where(clause = "status = 'Ativo'")
 public class Course {
 
 	@Id
@@ -20,10 +28,22 @@ public class Course {
 	@JsonProperty("_id")
 	private Long id;
 	
+	@NotBlank
+	@NotNull
+	@Length(min = 5, max = 100)
 	@Column(length = 200, nullable = false)
 	private String name;
 	
-	@Column(length = 50, nullable = false)
+	@NotNull
+	@Length(max = 10)
+	@Pattern(regexp = "Back-end|Front-end")
+	@Column(length = 10, nullable = false)
 	private String category;
+	
+	@NotNull
+	@Length(max = 10)
+	@Pattern(regexp = "Ativo|Inativo")
+	@Column(length = 10, nullable = false)
+	private String status = "Ativo";
 	
 }
